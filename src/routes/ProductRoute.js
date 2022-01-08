aa5a63f0533a24dc3a440208e6da9215cf388d8b
Asarvarjon <amriyevsarvarjon@yandex.com>
2022-01-08 18:12:11 +0500

    fixed bug

--- src/routes/ProductRoute.js
+++ src/routes/ProductRoute.js
@@ -3,6 +3,7 @@ const expressFileUpload = require("express-fileupload");
 const Authmiddleware = require("../middlewares/AuthMiddleware")
 const ProductRouter = require("express").Router()
 
+ProductRouter.use(Authmiddleware)
 
  
 ProductRouter.post("/", expressFileUpload(), ProductCreatePostController);
