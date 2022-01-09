0674c714fae946c8358d1fff683e34ad7afddeeb
abdiraximov97 <abdiraximovshaxboz97@gmail.com>
2022-01-09 13:54:37 +0500

    fix orderRoute

--- src/routes/OrderRoute.js
+++ src/routes/OrderRoute.js
@@ -1,9 +1,10 @@
 const { OrderPostController } = require("../controllers/OrderController");
 const { OrderGetController } = require("../controllers/OrderController");
+const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
 const OrderRouter = require("express").Router();
 
 OrderRouter.post("/", OrderPostController);
-OrderRouter.get("/", OrderGetController);
+OrderRouter.get("/", AuthMiddleware,  OrderGetController);
 
 module.exports = OrderRouter;
