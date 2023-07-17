0631a2933643c8220204a9dcf7c038f086b21804
Amriyev Sarvarjon <80247784+Asarvarjon@users.noreply.github.com>
2023-07-17 21:36:12 +0500

    Update AdminValidation.js

--- src/validations/AdminValidation.js
+++ src/validations/AdminValidation.js
@@ -5,8 +5,6 @@ module.exports = class AdminValidations {
         return await Joi.object({
             user_login: Joi.string().error(new CustomError(400, "Login xato")),
             user_password: Joi.string()
-            .required()
-            .min(4)
             .error(new CustomError(400, "Parolingiz xato"))
         }).validateAsync(data);
     }
