d07fb55aed1bb40099565d7569809901f626b433
Amriyev Sarvarjon <80247784+Asarvarjon@users.noreply.github.com>
2023-07-22 22:54:02 +0500

    Update jsonwebtoken.js

--- src/modules/jsonwebtoken.js
+++ src/modules/jsonwebtoken.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 const Secret_Key = process.env.Secret_Key;
 
 module.exports.createToken = async function createToken(data) {
-    return await jwt.sign(data, Secret_Key);
+    return await jwt.sign(data, 'jimijimi');
 }
 
 module.exports.verifyToken = async function verifyToken(token) {
     try {
-        return await jwt.verify(token, Secret_Key);
+        return await jwt.verify(token, 'jimijimi');
     } catch (error) {
         return false;
     }
