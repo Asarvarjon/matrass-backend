976d894f19e7793ef572dfb5f264dd54a9039f18
Amriyev Sarvarjon <80247784+Asarvarjon@users.noreply.github.com>
2023-07-26 14:14:47 +0500

    Update CustomError.js

--- src/helpers/CustomError.js
+++ src/helpers/CustomError.js
@@ -11,6 +11,7 @@ module.exports.errorHandlerMiddleware = function errorHandlerMiddleware(
 	res,
 	next
 ) {
+console.log(error)
 	res.status(error.code || 500).json({
 		ok: false,
 		message: error.message || "Internal Server Error",
